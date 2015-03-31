7129cac4d8e5ef892af185aa7be4048c29c86fc9
Dillon Welch <daw0328@gmail.com>
2015-03-31 11:43:33 -0700

    [#14] Make indexes unique.

--- db/migrate/20150331035425_create_champion.rb
+++ db/migrate/20150331035425_create_champion.rb
@@ -1,9 +1,13 @@
 class CreateChampion < ActiveRecord::Migration
   def change
     create_table :champions do |t|
-      t.integer :riot_id, index: true
-      t.string  :name,    index: true
+      t.integer :riot_id
+      t.index   :riot_id, unique: true
+      t.string  :name
+      t.index   :name, unique: true
       t.json    :raw_api_data
+
+      t.timestamps
     end
   end
 end
