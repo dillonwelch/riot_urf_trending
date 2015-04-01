a7700e93eca3f8e65c063608bf9e327b8bc48440
Dillon Welch <daw0328@gmail.com>
2015-03-31 19:45:02 -0700

    Add group by

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @victories = ChampionMatch.joins(:champion).where(victory: true).select(:champion_id, :name).group(:champion_id, :name).select('count(victory) as victories')
-    @losses = ChampionMatch.joins(:champion).where(victory: false).select(:champion_id, :name).group(:champion_id, :name).select('count(victory) as losses')
-    # @merged = @victories.merge(@losses)
     @merged = ChampionMatch.find_by_sql('
       select coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name
       from champion_matches
@@ -22,9 +19,9 @@ class ApplicationController < ActionController::Base
           where victory = true
           group by champion_id
       ) as victories on victories.champion_id = champion_matches.champion_id
-      -- TODO duplicate entries with multiple champion match entries
       right outer join champions on champion_matches.champion_id = champions.id
+      group by name, victories, losses
       order by champions.name
-      ')
+    ')
   end
 end
