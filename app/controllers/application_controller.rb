5730fcbe8e42a639216d2840f15dfde2ab7551bb
Dillon Welch <daw0328@gmail.com>
2015-03-31 19:48:19 -0700

    Move query to ChampionMatch model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,24 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    @merged = ChampionMatch.find_by_sql('
-      select coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name
-      from champion_matches
-      left outer join (
-          select count(victory) as losses, champion_id
-          from champion_matches
-          where victory = false
-          group by champion_id
-      ) as losses on losses.champion_id = champion_matches.champion_id
-      left outer join (
-          select count(victory) as victories, champion_id
-          from champion_matches
-          where victory = true
-          group by champion_id
-      ) as victories on victories.champion_id = champion_matches.champion_id
-      right outer join champions on champion_matches.champion_id = champions.id
-      group by name, victories, losses
-      order by champions.name
-    ')
+    @merged = ChampionMatch.all_wins_and_losses
   end
 end
