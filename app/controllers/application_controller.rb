8b15b6f26d5e4c487c0637e73303ce5950868452
Dillon Welch <daw0328@gmail.com>
2015-03-31 17:26:46 -0700

    Add basic view displaying all win and loss data.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @victories = ChampionMatch.joins(:champion).where(victory: true).select(:champion_id, :name).group(:champion_id, :name).select('count(victory) as victories')
+    @losses = ChampionMatch.joins(:champion).where(victory: false).select(:champion_id, :name).group(:champion_id, :name).select('count(victory) as losses')
+  end
 end
