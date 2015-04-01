dff3a7d300fa285f8022e74e91b874958bb16399
Dillon Welch <daw0328@gmail.com>
2015-03-31 19:54:46 -0700

    [#31] Queries to get win rates and most played. Convert queries to ActiveRecord

--- app/models/champion_match.rb
+++ app/models/champion_match.rb
@@ -18,25 +18,53 @@ class ChampionMatch < ActiveRecord::Base
     joins(:champion).where(champions: { riot_id: riot_id })
   end
 
-  def self.all_wins_and_losses
-    find_by_sql('
-      select coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name
-      from champion_matches
+  def self.all_losses
+    joins('
       left outer join (
-          select count(victory) as losses, champion_id
-          from champion_matches
-          where victory = false
-          group by champion_id
+        select count(victory) as losses, champion_id
+        from champion_matches
+        where victory = false
+        group by champion_id
       ) as losses on losses.champion_id = champion_matches.champion_id
+      '
+    )
+  end
+
+  def self.all_wins
+    joins('
       left outer join (
-          select count(victory) as victories, champion_id
-          from champion_matches
-          where victory = true
-          group by champion_id
+        select count(victory) as victories, champion_id
+        from champion_matches
+        where victory = true
+        group by champion_id
       ) as victories on victories.champion_id = champion_matches.champion_id
-      right outer join champions on champion_matches.champion_id = champions.id
-      group by name, victories, losses
-      order by champions.name
-    ')
+      '
+    )
+  end
+
+  def self.all_wins_and_losses
+    select('coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name').
+      all_wins.all_losses.
+      joins('right outer join champions on champion_matches.champion_id = champions.id').
+      group('name, victories, losses').
+      order('name asc')
+  end
+
+  def self.n_most_played(n=5)
+    select('coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name, coalesce((sum(victories + losses) / count(name))::int, 0) as total').
+      all_wins.all_losses.
+      joins('right outer join champions on champion_matches.champion_id = champions.id').
+      group('name, victories, losses').
+      order('total desc').
+      limit(n)
+  end
+
+  def self.n_best(n=5)
+    select('coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name, coalesce((victories::float / count(name)), 0) as win_rate').
+      all_wins.all_losses.
+      joins('right outer join champions on champion_matches.champion_id = champions.id').
+      group('name, victories, losses').
+      order('win_rate desc').
+      limit(n)
   end
 end
