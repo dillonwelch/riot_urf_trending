ae89c4b55dbc4acf15d021271b87ffdc1d1f3056
Dillon Welch <daw0328@gmail.com>
2015-04-01 23:43:59 -0700

    [#2] Add clockwork gem and setup for heroku.

---
+++ lib/clock.rb
@@ -0,0 +1,17 @@
+require './config/boot'
+require './config/environment'
+require 'clockwork'
+
+def execute_rake(task, file = 'riot_api_tasks.rake')
+  require 'rake'
+  rake = Rake::Application.new
+  Rake.application = rake
+  Rake::Task.define_task(:environment)
+  load "#{Rails.root}/lib/tasks/#{file}"
+  rake[task].reenable
+  rake[task].invoke
+end
+
+module Clockwork
+  every(5.minutes, 'get urf matches') { execute_rake('get_urf_matches') }
+end
