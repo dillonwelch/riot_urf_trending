e81178ed6bf3a6942b68715f2ce1aeff9530dc0a
Dillon Welch <daw0328@gmail.com>
2015-04-01 20:30:02 -0700

    Update query to use correct math.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def best_win_rate_with_history
     hours = params[:hours].to_i
-    best = ChampionMatch.n_best(5, Time.zone.now - 5.hours)
+    best = ChampionMatch.n_best(5, Time.zone.now - 1.hours)
     final_data = {}
     best.each do |champion|
       final_data[champion.name] = {}
