8eec1f1c01f572f39cfba0fa7d197dd9e547c2d9
Dillon Welch <daw0328@gmail.com>
2015-04-04 03:54:59 -0700

    [#44] Drop api data from original tables.

--- db/migrate/20150404103150_create_raw_api_tables.rb
+++ db/migrate/20150404103150_create_raw_api_tables.rb
@@ -16,5 +16,24 @@ class CreateRawApiTables < ActiveRecord::Migration
 
     add_foreign_key :champion_api_data, :champions, name: 'fk_rails_champion_api_data_champions', on_delete: :cascade
     add_foreign_key :match_api_data, :matches, name: 'fk_rails_match_api_data_matches', on_delete: :cascade
+
+    execute %{
+      insert into champion_api_data
+      (champion_id, raw_api_data, created_at, updated_at)
+        (
+          select id, raw_api_data, created_at, updated_at
+          from champions
+        );
+
+      insert into match_api_data
+      (match_id, raw_api_data, created_at, updated_at)
+        (
+          select id, raw_api_data, created_at, updated_at
+          from matches
+        );
+    }
+
+    remove_column :champions, :raw_api_data
+    remove_column :matches, :raw_api_data
   end
 end
