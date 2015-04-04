e0b51ed7f839682cceaae956b82be5b6a02fc1e0
Dillon Welch <daw0328@gmail.com>
2015-04-03 18:44:30 -0700

    [#58] Add counters for total matches, total kills, and total deaths.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   get '/best_win_rate_with_history', to: 'application#best_win_rate_with_history'
 
   namespace :api do
-    get '/champions/:name/kills', to: 'champion#kills'
+    get '/champions/total_kills',  to: 'champion#total_kills'
+    get '/champions/total_deaths', to: 'champion#total_deaths'
+
+    get '/champions/:name/kills',  to: 'champion#kills'
     get '/champions/:name/deaths', to: 'champion#deaths'
+
+    get '/matches/total', to: 'match#total'
   end
 end
