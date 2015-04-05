b87d37d877ccbd92dc5d448f2b3028aacfb36428
Dillon Welch <daw0328@gmail.com>
2015-04-05 03:00:04 -0700

    Fix style issues.

--- app/controllers/api/champions_controller.rb
+++ app/controllers/api/champions_controller.rb
@@ -17,8 +17,7 @@ module Api
     end
 
     def best_win_rate_with_history
-      # Time.zone.now = 2:08PM => 1:00PM
-      time = Time.at ( ((Time.zone.now - 1.hour).to_f / 1.hour).floor * 1.hour)
+      time = rounded_previous_hour
 
       best = Rails.cache.fetch("n_best_5_#{time}") do
         ChampionMatch.n_best(5, time).to_a
@@ -26,29 +25,42 @@ module Api
 
       ids = best.map(&:champion_id)
       champions = Champion.where(id: ids).pluck(:id, :name).to_h
-      final_data = {}
 
-      best.each do |champion|
-        name = champions[champion.champion_id]
-        final_data[name] = {}
-
-        cache_key = "ChampionHistoryQuery_#{champion.champion_id}_#{time}"
-        result = Rails.cache.fetch(cache_key) do
-          ChampionHistoryQuery.new(champion_id: champion.champion_id,
-                                   start_time: time).run.to_a
-        end
-
-        result.each do |hour|
-          final_data[name][hour['time']] = hour['win_rate']
-        end
-      end
-      render json: final_data.to_json
+      render json: history_data(best, time, champions)
     end
 
     private
 
     def champion
       @champion ||= Champion.find_by_name(params[:name])
+    end
+
+    def rounded_previous_hour
+      # Time.zone.now = 2:08PM => 1:00PM
+      Time.at ( ((Time.zone.now - 1.hour).to_f / 1.hour).floor * 1.hour)
+    end
+
+    def champion_history(champion, time)
+      cache_key = "ChampionHistoryQuery_#{champion.champion_id}_#{time}"
+      Rails.cache.fetch(cache_key) do
+        ChampionHistoryQuery.new(champion_id: champion.champion_id,
+                                 start_time: time).run.to_a
+      end
+    end
+
+    def history_data(best, time, champions)
+      final_data = {}
+
+      best.each do |champion|
+        name = champions[champion.champion_id]
+        final_data[name] = {}
+
+        champion_history(champion, time).each do |hour|
+          final_data[name][hour['time']] = hour['win_rate']
+        end
+      end
+
+      final_data.to_json
     end
   end
 end
--- app/models/champion_match.rb
+++ app/models/champion_match.rb
@@ -22,7 +22,8 @@ class ChampionMatch < ActiveRecord::Base
     joins(:champion).where(champions: { riot_id: riot_id })
   end
 
-  def self.all_losses(start_time=Time.zone.now - 5.minutes, end_time=Time.zone.now)
+  def self.all_losses(start_time=Time.zone.now - 5.minutes,
+                      end_time=Time.zone.now)
     joins(
       sanitize_sql_array(
         [
@@ -35,14 +36,15 @@ class ChampionMatch < ActiveRecord::Base
              and ( ( start_time + duration * 1000) < :end_time )
              group by champion_id
            ) as losses on losses.champion_id = champion_matches.champion_id',
-           start_time: start_time.to_i * 1000,
-           end_time: end_time.to_i * 1000
+          start_time: start_time.to_i * 1000,
+          end_time: end_time.to_i * 1000
         ]
       )
     )
   end
 
-  def self.all_wins(start_time=Time.zone.now - 5.minutes, end_time=Time.zone.now)
+  def self.all_wins(start_time=Time.zone.now - 5.minutes,
+                    end_time=Time.zone.now)
     joins(
       sanitize_sql_array(
         [
@@ -54,20 +56,24 @@ class ChampionMatch < ActiveRecord::Base
              and ( ( start_time + duration * 1000) >= :start_time )
              and ( ( start_time + duration * 1000) < :end_time )
              group by champion_id
-           ) as victories on victories.champion_id = champion_matches.champion_id',
-           start_time: start_time.to_i * 1000,
-           end_time: end_time.to_i * 1000
+           ) as victories on
+            victories.champion_id = champion_matches.champion_id',
+          start_time: start_time.to_i * 1000,
+          end_time: end_time.to_i * 1000
         ]
       )
     )
   end
 
-  def self.wins_and_losses(start_time=Time.zone.now - 6.days, end_time=Time.zone.now)
-    all_wins(start_time, end_time).all_losses(start_time, end_time)#.joins('right outer join champions on champion_matches.champion_id = champions.id')
+  def self.wins_and_losses(start_time=Time.zone.now - 6.days,
+                           end_time=Time.zone.now)
+    all_wins(start_time, end_time).all_losses(start_time, end_time)
   end
 
-  # def self.all_wins_and_losses(start_time=Time.zone.now - 6.days, end_time=Time.zone.now)
-  #   select('coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name').
+  # def self.all_wins_and_losses(start_time=Time.zone.now - 6.days,
+  #                              end_time=Time.zone.now)
+  #   select('coalesce(victories, 0) as victories,
+  #          coalesce(losses, 0) as losses, name').
   #     joins(:match).where(
   #       '( ( start_time + duration * 1000) >= :start_time )
   #        and ( ( start_time + duration * 1000) < :end_time )',
@@ -78,8 +84,13 @@ class ChampionMatch < ActiveRecord::Base
   #     order('name asc')
   # end
 
-  # def self.n_most_played(n=5, start_time=Time.zone.now - 6.days, end_time=Time.zone.now)
-  #   select('coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, name, coalesce((sum(victories + losses) / count(name))::int, 0) as total').
+  # def self.n_most_played(n=5,
+  #                        start_time=Time.zone.now - 6.days
+  #                        end_time=Time.zone.now)
+  #   select('coalesce(victories, 0) as victories,
+  #           coalesce(losses, 0) as losses,
+  #           name, coalesce((sum(victories + losses) /
+  #                           count(name))::int, 0) as total').
   #     joins(:match).where(
   #       '( ( start_time + duration * 1000) >= :start_time )
   #        and ( ( start_time + duration * 1000) < :end_time )',
@@ -91,16 +102,21 @@ class ChampionMatch < ActiveRecord::Base
   #     limit(n)
   # end
 
-  def self.n_best(n=5, start_time=Time.zone.now - 6.days, end_time=start_time + 1.hour)
-    select('coalesce(victories, 0) as victories, coalesce(losses, 0) as losses, champion_matches.champion_id, coalesce((victories::float / (victories + coalesce(losses, 0))), 0) as win_rate').
+  def self.n_best(n=5,
+                  start_time=Time.zone.now - 6.days,
+                  end_time=start_time + 1.hour)
+    select('coalesce(victories, 0) as victories,
+           coalesce(losses, 0) as losses,
+           champion_matches.champion_id,
+           coalesce((victories::float /
+                     (victories + coalesce(losses, 0))), 0) as win_rate').
       joins(:match).where(
         '( ( start_time + duration * 1000) >= :start_time )
          and ( ( start_time + duration * 1000) < :end_time )',
-         start_time: start_time.to_i * 1000,
-         end_time: end_time.to_i * 1000).
+        start_time: start_time.to_i * 1000,
+        end_time: end_time.to_i * 1000).
       wins_and_losses(start_time, end_time).
       group('champion_matches.champion_id, victories, losses').
-      order('win_rate desc, victories desc').
-      limit(n)
+      order('win_rate desc, victories desc').limit(n)
   end
 end
