91516da5f4c710117d15499222ab0972d99be5f3
Dillon Welch <daw0328@gmail.com>
2015-04-05 13:16:48 -0700

    [#82] Add feature specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+Capybara.javascript_driver = :poltergeist
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -7,5 +9,9 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.before type: :feature do
+    config.alias_example_group_to :feature, capybara_feature: true
   end
 end
