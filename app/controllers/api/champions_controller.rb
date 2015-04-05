d7461967e5e2214b72be2ed6d1a5af52830e6f63
Dillon Welch <daw0328@gmail.com>
2015-04-05 02:48:35 -0700

    Use Rails.cache.fetch blocks.

--- app/controllers/api/champions_controller.rb
+++ app/controllers/api/champions_controller.rb
@@ -20,16 +20,12 @@ module Api
       # Time.zone.now = 2:08PM => 1:00PM
       time = Time.at ( ((Time.zone.now - 1.hour).to_f / 1.hour).floor * 1.hour)
 
-      cache_key = "n_best_5_#{time}"
-      best = Rails.cache.read(cache_key)
-
-      # Look at block
-      if best.nil?
-        best = ChampionMatch.n_best(5, time)
-        Rails.cache.write(cache_key, best.to_a)
+      best = Rails.cache.fetch("n_best_5_#{time}") do
+        ChampionMatch.n_best(5, time).to_a
       end
 
-      champions = Champion.where(id: best.map(&:champion_id)).pluck(:id, :name).to_h
+      ids = best.map(&:champion_id)
+      champions = Champion.where(id: ids).pluck(:id, :name).to_h
       final_data = {}
 
       best.each do |champion|
@@ -37,11 +33,9 @@ module Api
         final_data[name] = {}
 
         cache_key = "ChampionHistoryQuery_#{champion.champion_id}_#{time}"
-        result = Rails.cache.read(cache_key)
-
-        if result.nil?
-          result = ChampionHistoryQuery.new(champion_id: champion.champion_id, start_time: time).run
-          Rails.cache.write(cache_key, result.to_a)
+        result = Rails.cache.fetch(cache_key) do
+          ChampionHistoryQuery.new(champion_id: champion.champion_id,
+                                   start_time: time).run.to_a
         end
 
         result.each do |hour|
