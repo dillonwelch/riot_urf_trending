a99cb632a1a2e0ef05001a0e200f5a97fee5d494
Dillon Welch <daw0328@gmail.com>
2015-04-05 12:39:19 -0700

    [#81] Add spec for ChampionHistoryQuery.

--- app/queries/champion_history_query.rb
+++ app/queries/champion_history_query.rb
@@ -51,7 +51,7 @@ class ChampionHistoryQuery
               and ( ( start_time + duration * 1000) >= :start_time )
               and ( ( start_time + duration * 1000) < :end_time )
               group by champion_id
-          ) as losses on losses.champion_id = victories.champion_id
+          ) as losses on losses.champion_id = champion_matches.champion_id
           where champion_matches.champion_id = :champion_id
           GROUP BY champion_matches.champion_id, victories, losses
         ),
