4b4b423712c02e32c1b252d8923670e9b74cf949
Dillon Welch <daw0328@gmail.com>
2015-04-10 21:55:22 -0700

    [#45] Add specs for new finder.

--- spec/models/champion_spec.rb
+++ spec/models/champion_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe Champion do
   it { expect(subject).to have_db_index(:riot_id) }
   it { expect(subject).to have_db_index(:primary_role) }
   it { expect(subject).to have_db_index(:secondary_role) }
+
+  describe '.find_by_lower_name' do
+    let!(:champion) { create(:champion, name: 'Amumu') }
+
+    it 'finds the exact name' do
+      expect(Champion.find_by_lower_name('Amumu')).to eq [champion]
+    end
+
+    it 'finds the all lower case name' do
+      expect(Champion.find_by_lower_name('amumu')).to eq [champion]
+    end
+
+    it 'finds the all upper case name' do
+      expect(Champion.find_by_lower_name('AMUMU')).to eq [champion]
+    end
+
+    it 'does not find with the wrong name' do
+      expect(Champion.find_by_lower_name('Amumu!')).to eq []
+    end
+  end
 end
