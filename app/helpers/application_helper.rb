bf6e29ce64a094d7175673308f5528365d85aca1
Dillon Welch <daw0328@gmail.com>
2015-04-11 19:41:38 -0700

    Refactor transform_display_name.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,26 +8,23 @@ module ApplicationHelper
 
   def transform_display_name(display_name)
     display_name.gsub!(/ /, '')
-    if display_name == "Kog'Maw"
-      'KogMaw'
-    elsif display_name == "Kha'Zix"
-      'Khazix'
-    elsif display_name == "Rek'Sai"
-      'RekSai'
-    elsif display_name == "Cho'Gath"
-      'Chogath'
-    elsif display_name == "Vel'Koz"
-      'Velkoz'
-    elsif display_name == 'Dr.Mundo'
-      'DrMundo'
-    elsif display_name == 'LeBlanc'
-      'Leblanc'
-    elsif display_name == 'Fiddlesticks'
-      'FiddleSticks'
-    elsif display_name == 'Wukong'
-      'MonkeyKing'
-    else
-      display_name
+
+    hash = {
+      "Kog'Maw"      => 'KogMaw',
+      "Kha'Zix"      => 'Khazix',
+      "Rek'Sai"      => 'RekSai',
+      "Cho'Gath"     => 'Chogath',
+      "Vel'Koz"      => 'Velkoz',
+      'Dr.Mundo'     => 'DrMundo',
+      'LeBlanc'      => 'Leblanc',
+      'Fiddlesticks' => 'FiddleSticks',
+      'Wukong'       => 'MonkeyKing'
+    }
+
+    begin
+      hash.fetch(display_name)
+    rescue KeyError
+      return display_name
     end
   end
 end
