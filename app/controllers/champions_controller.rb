9a370420154fd5a1a332b2980de5dd6302e7121f
Dillon Welch <daw0328@gmail.com>
2015-04-12 20:29:54 -0700

    Add average win/pick rates to show page.

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -41,13 +41,23 @@ class ChampionsController < ApplicationController
   def search
     begin
       @champion = ChampionMatchesStat.select(
-        '(sum(victories)::float / sum(victories + losses)) as win_rate,
+        '(sum(victories)::float / sum(victories + losses)) * 100 as win_rate,
         sum(victories + losses)::float / (
           select sum(victories + losses) from champion_matches_stats
-        ) as pick_rate,
+        ) * 100 as pick_rate,
         champion_id, name'
       ).joins(:champion).where(champion_id: champion.id).
-      group(:champion_id, :name).reorder('win_rate desc').first
+      group(:champion_id, :name).reorder('').first
+
+      @champions = ChampionMatchesStat.select(
+        '(sum(victories)::float / sum(victories + losses)) * 100 as win_rate,
+        champion_id'
+      ).joins(:champion).group(:champion_id)
+
+      win_rates = @champions.map(&:win_rate)
+      @average_win_rate = win_rates.sum / win_rates.size
+      @average_pick_rate = 100.0 / @champions.size
+
     rescue NoMethodError
       @name = params[:name]
       render 'empty_search' and return
