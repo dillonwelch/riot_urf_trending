0f560d7d26c2437970f2ae7aff2b6bd466d83d00
Dillon Welch <daw0328@gmail.com>
2015-04-13 16:15:26 -0700

    [#125] Finished writing specs.

--- spec/models/champion_matches_stat_spec.rb
+++ spec/models/champion_matches_stat_spec.rb
@@ -15,47 +15,46 @@ RSpec.describe ChampionMatchesStat do
     let!(:stat1) do
       create(:champion_matches_stat,
              champion: champion,
-             start_time: Time.parse('2015 Apr 01').to_i * 1000)
-     end
+             start_time: Time.parse('2015 Apr 01 11:00').to_i * 1000)
+    end
     let!(:stat2) do
       create(:champion_matches_stat,
              champion: champion,
-             start_time: Time.parse('2015 Apr 01').to_i * 1000)
-     end
+             start_time: Time.parse('2015 Apr 01 12:00').to_i * 1000)
+    end
     let!(:statOther1) do
       create(:champion_matches_stat,
-             start_time: Time.parse('2015 Apr 01').to_i * 1000)
-    end
-    let(:april_win_rate) do
-      (stat1.victories + stat2.victories).to_f / (stat1.victories + stat1.losses + stat2.victories + stat2.losses) * 100
-    end
-    let(:april_pick_rate) do
-      (stat1.victories + stat1.losses + stat2.victories + stat2.losses).to_f / april_total_picks * 100
-    end
-    let(:april_total_picks) do
-      stat1.victories + stat1.losses + stat2.victories + stat2.losses + statOther1.victories + statOther1.losses
+             start_time: Time.parse('2015 Apr 01 12:00').to_i * 1000)
     end
     let!(:stat3) do
       create(:champion_matches_stat,
              champion: champion,
              start_time: Time.parse('2015 May 31').to_i * 1000)
-     end
+    end
     let(:result) { ChampionMatchesStat.overall_for_champion(champion) }
-    let(:may_win_rate) do
-      stat3.victories.to_f / (stat3.victories + stat3.losses) * 100
-    end
-    let(:may_pick_rate) do
-      (stat3.victories + stat3.losses).to_f / total_picks * 100
-    end
 
     describe 'first month' do
+      let(:win_rate) do
+        ((stat1.victories + stat2.victories).to_f) /
+          (stat1.victories + stat1.losses + stat2.victories + stat2.losses) *
+          100
+      end
+      let(:pick_rate) do
+        ((stat1.victories + stat1.losses +
+          stat2.victories + stat2.losses).to_f) /
+          total_picks * 100
+      end
+      let(:total_picks) do
+        stat1.victories + stat1.losses + stat2.victories + stat2.losses +
+          statOther1.victories + statOther1.losses
+      end
+
       it 'calculates win rates for the first month' do
-        expect(result.first.win_rate).to be_within(0.0005).of april_win_rate
+        expect(result.first.win_rate).to be_within(0.0005).of win_rate
       end
 
       it 'calculates pick rates for the first month' do
-        byebug
-        expect(result.first.pick_rate).to be_within(0.0005).of april_pick_rate
+        expect(result.first.pick_rate).to be_within(0.0005).of pick_rate
       end
 
       it 'extracts the month for the first month' do
@@ -68,8 +67,12 @@ RSpec.describe ChampionMatchesStat do
     end
 
     describe 'second month' do
+      let(:win_rate) do
+        stat3.victories.to_f / (stat3.victories + stat3.losses) * 100
+      end
+
       it 'calculates win rates for the second month' do
-        expect(result.second.win_rate).to be_within(0.0005).of may_win_rate
+        expect(result.second.win_rate).to be_within(0.0005).of win_rate
       end
 
       it 'calculates pick rates for the second month' do
