f1676031fc7eb0ddbc2764562048bfcbc3c93c14
Dillon Welch <daw0328@gmail.com>
2015-04-14 11:40:52 -0700

    [#127] Style All Roles page.

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -87,10 +87,10 @@ class ChampionsController < ApplicationController
 
   def primary_role
     @roles = ChampionMatchesStat.select(
-      '(sum(victories)::float / sum(victories + losses)) as win_rate,
+      '(sum(victories)::float / sum(victories + losses)) * 100 as win_rate,
       sum(victories + losses)::float / (
         select sum(victories + losses) from champion_matches_stats
-      ) as pick_rate,
+      ) * 100 as pick_rate,
       primary_role'
     ).joins(:champion).group(:primary_role).reorder('win_rate desc')
   end
