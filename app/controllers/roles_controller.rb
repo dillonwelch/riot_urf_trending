edbc6dc180629928650aa3acf1cc4d9efd16c5dc
Dillon Welch <daw0328@gmail.com>
2015-04-16 20:42:37 -0700

    Random style fixes.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,12 +2,12 @@ class RolesController < ApplicationController
   def index
     latest = Rails.cache.fetch("max_created_at_#{ENV['CACHE_COUNTER']}") do
       ChampionMatchesStat.select('max(created_at) as created_at').
-       reorder('').first.created_at
+      reorder('').first.created_at
     end
 
     @roles = Rails.cache.fetch("stats_roles_index_#{latest}") do
       ChampionMatchesStat.all_champion_stats.select('primary_role').
-        group(:primary_role).reorder('win_rate desc').to_a
+      group(:primary_role).reorder('win_rate desc').to_a
     end
   end
 
