1ba67dab835db4ed47ef076cc8fdb16474b726b5
Dillon Welch <daw0328@gmail.com>
2015-04-17 20:58:39 -0700

    [#107] Finish spec for layout.

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -5,14 +5,58 @@ RSpec.describe 'layouts/application' do
     render
   end
 
+  def contain_link(link, text)
+    have_css("a[href='#{link}']", text: text)
+  end
+
+  def text(key)
+    I18n.t("layouts.application.#{key}")
+  end
+
   it 'has the HTML title' do
-    expect(response).to have_css('title',
-                                 text: I18n.t('layouts.application.title'),
-                                 visible: false)
+    expect(response).to have_css('title', text: text('title'), visible: false)
   end
 
   it 'has a link to the home page' do
-    expect(response).to have_css("a[href='#{root_path}']",
-                                 text: I18n.t('layouts.application.home'))
+    expect(response).to contain_link(root_path, text('home'))
+  end
+
+  it 'has a link to the roles index page' do
+    expect(response).to contain_link(roles_path, text('roles_index'))
+  end
+
+  I18n.t('layouts.application.roles').each do |param, display_name|
+    it "has a link to #{display_name}" do
+      expect(response).
+        to contain_link(champions_path(role: param), display_name)
+    end
+  end
+
+  it 'has a link to the champion index page' do
+    expect(response).to contain_link(champions_path, text('champions_index'))
+  end
+
+  I18n.t('layouts.application.overunder').each do |param, display_name|
+    it "has a link to #{display_name}" do
+      expect(response).
+        to contain_link(champions_path(rated: param), display_name)
+    end
+  end
+
+  it 'has a link to the URF awards page' do
+    expect(response).to contain_link(urf_awards_path, text('urf_awards'))
+  end
+
+  it 'has a link to the spreadsheet' do
+    expect(response).to contain_link(ENV['DOWNLOAD_LINK'], text('download'))
+  end
+
+  it 'has a contact email link' do
+    expect(response).
+      to contain_link("mailto:#{ENV['CONTACT_EMAIL']}", text('contact'))
+  end
+
+  it 'has a link to the about page' do
+    expect(response).to contain_link(about_path, text('about'))
   end
 end
