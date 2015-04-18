b9602c275e47ab1d9e60acda88087481c42d665e
Dillon Welch <daw0328@gmail.com>
2015-04-17 21:39:32 -0700

    [#107 #135] Specs for ChampionsController.

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -3,57 +3,14 @@ class ChampionsController < ApplicationController
     @champions = ChampionMatchesStat.all_champion_stats.
                  select('champion_id, name').group(:champion_id, :name)
 
-    if params[:order] == 'win_rate' || params[:order].nil?
-      if params[:asc] == 'true'
-        @champions = @champions.reorder('win_rate asc')
-      else
-        @champions = @champions.reorder('win_rate desc')
-      end
-    elsif params[:order] == 'pick_rate'
-      if params[:asc] == 'true'
-        @champions = @champions.reorder('pick_rate asc')
-      else
-        @champions = @champions.reorder('pick_rate desc')
-      end
-    elsif params[:order] == 'name'
-      if params[:asc] == 'true'
-        @champions = @champions.reorder('name asc')
-      else
-        @champions = @champions.reorder('name desc')
-      end
-    end
+    filter_by_params
 
-    if params[:role].present?
-      @champions = @champions.where('primary_role = ?', params[:role])
-    end
-
-    latest = Rails.cache.fetch("max_created_at_#{ENV['CACHE_COUNTER']}") do
-      ChampionMatchesStat.select('max(created_at) as created_at').
-      reorder('').first.created_at
-    end
-    my_params = "#{params[:role]}_#{params[:order]}_#{params[:asc]}"
-
-    @champions = Rails.cache.fetch("stats_index_#{latest}_#{my_params}") do
+    @champions = Rails.cache.fetch(index_key) do
       @champions.to_a
     end
 
-    @bans = bans
-
-    average_win_and_pick_rates
-
-    if params[:rated].present?
-      if params[:rated] == 'over'
-        @champions.select! do |champion|
-          champion.win_rate < @average_win_rate &&
-            champion.pick_rate > @average_pick_rate
-        end
-      elsif params[:rated] == 'under'
-        @champions.select! do |champion|
-          champion.win_rate > @average_win_rate &&
-            champion.pick_rate < @average_pick_rate
-        end
-      end
-    end
+    average_rates
+    filter_by_rated
 
     respond_to do |format|
       format.js { render '_all_champions', layout: false }
@@ -66,19 +23,9 @@ class ChampionsController < ApplicationController
       ChampionMatchesStat.individual_champion_stats(champion)
     end
 
-    @champions = Rails.cache.fetch('stats_show_all_champions') do
-      ChampionMatchesStat.select(
-        '(sum(victories)::float / sum(victories + losses)) * 100 as win_rate,
-          sum(victories + losses)::float / (
-            select sum(victories + losses) from champion_matches_stats
-          ) * 1000 as pick_rate,
-        champion_id'
-      ).joins(:champion).group(:champion_id).to_a
-    end
+    @champions = stats_show_all_champions
 
-    @bans = bans
-
-    average_win_and_pick_rates
+    average_rates
 
   rescue NoMethodError
     @name = params[:name]
@@ -104,15 +51,92 @@ class ChampionsController < ApplicationController
     end
   end
 
-  def average_win_and_pick_rates
+  def average_rates
     win_rates = @champions.map(&:win_rate)
     @average_win_rate = win_rates.sum / win_rates.size
     pick_rates = @champions.map(&:pick_rate)
     @average_pick_rate = pick_rates.sum / pick_rates.size
+    @bans = bans
   end
 
   def rounded_previous_hour
     # Time.zone.now = 2:08PM => 1:00PM
     Time.at ( ((Time.zone.now - 1.hour).to_f / 1.hour).floor * 1.hour)
+  end
+
+  def filter_by_params
+    case params[:order]
+    when 'win_rate'
+      filter_asc('win_rate')
+    when 'pick_rate'
+      filter_asc('pick_rate')
+    when 'name'
+      filter_asc('name')
+    else
+      filter_asc('win_rate')
+    end
+
+    return unless params[:role].present?
+
+    @champions = @champions.where('primary_role = ?', params[:role])
+  end
+
+  def filter_asc(order)
+    if params[:asc] == 'true'
+      @champions = @champions.reorder("#{order} asc")
+    else
+      @champions = @champions.reorder("#{order} desc")
+    end
+  end
+
+  def cache_key
+    "#{params[:role]}_#{params[:order]}_#{params[:asc]}"
+  end
+
+  def index_key
+    "stats_index_#{max_created_at}_#{cache_key}"
+  end
+
+  def max_created_at
+    @_max = Rails.cache.fetch("max_created_at_#{ENV['CACHE_COUNTER']}") do
+      ChampionMatchesStat.select('max(created_at) as created_at').
+      reorder('').first.created_at
+    end
+  end
+
+  def overrated?(champion)
+    champion.win_rate < @average_win_rate &&
+      champion.pick_rate > @average_pick_rate
+  end
+
+  def underrated?(champion)
+    champion.win_rate > @average_win_rate &&
+      champion.pick_rate < @average_pick_rate
+  end
+
+  def filter_by_rated
+    return unless params[:rated].present?
+
+    if params[:rated] == 'over'
+      @champions.select! do |champion|
+        overrated?(champion)
+      end
+    elsif params[:rated] == 'under'
+      @champions.select! do |champion|
+        underrated?(champion)
+      end
+    end
+  end
+
+  def stats_show_all_champions
+    Rails.cache.fetch('stats_show_all_champions') do
+      ChampionMatchesStat.select(
+        '(sum(victories)::float / sum(victories + losses)) * 100 as win_rate,
+          sum(victories + losses)::float / (
+            select sum(victories + losses) from champion_matches_stats
+          ) * 1000 as pick_rate,
+        champion_id'
+      ).joins(:champion).group(:champion_id).to_a
+    end
   end
 end
